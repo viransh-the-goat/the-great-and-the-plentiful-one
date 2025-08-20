86da732bb7b8fd1a8b6bd5f0fff2e89e841f4967
viransh-the-goat <viransh5357@ggn.hxls.org>
2025-08-20 15:30:48 +0530

    Update tweets.py

--- tweets.py
+++ tweets.py
@@ -10,20 +10,18 @@ os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
 model = Google(model = "gemini-1.5-flash-latest")
 
 tweet_template = """
-Give me {number} tweets on {topic} in {language}.
+Give me {language} in {number} about {topic}
 Please follow the below instructions:
-1. Do not translate to English if the given language is not English.
-2. If {language} is empty or not a real language, default to English and ignore {language}.
-3. If {topic} is empty or does not make sense, then respond with "Please enter a topic for your tweet!".
-4. Avoid unnecessary indents or spaces.
-5. Keep the tweets short, but include at least 1 hashtag per tweet.
+The main game is roblox the game are games inside of roblox
+give answer to the question{language} 
+
 """
 tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['number', 'topic', 'language'])
 
 with st.form(key = 'tweets'):
-    topic = st.text_input("Topic: ")
-    number = st.number_input("Number of Tweets: ", value = 1, step = 1, max_value = 10, min_value = 1)
-    language = st.text_input("Language: ")
+    topic = st.text_input("Game(in roblox): ")
+    number = st.number_input("Number of results: ", value = 1, step = 1, max_value = 10, min_value = 1)
+    language = st.text_input("Question: ")
     submit = st.form_submit_button("Generate")
 
 if submit:
